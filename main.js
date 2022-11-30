993af1596cd8b4163b4b5b016877cb354ba9b11c
choijaeho <dhqkgbs@naver.com>
2022-11-30 21:22:42 +0900

    feat : 점수변경

--- main.js
+++ main.js
@@ -136,6 +136,9 @@ function arrayPlusArray(arr1, arr2) {
     for(let i in newArr) {
         newArr[i] =  arr1[i] + arr2[i];
     }
+    if(progressCnt ==5 ){
+        alert("결과 arr1 [" + arr1 + "] + arr2 [" + arr2 + "] = " + newArr);
+    }
     console.log("결과 arr1 [" + arr1 + "] + arr2 [" + arr2 + "] = " + newArr);
     return newArr;
 }
@@ -336,3 +339,5 @@ function tutorial_init(){
     $("#t4_modal").css( 'display', 'none');
     $("#t5_modal").css( 'display', 'none');
 }
+
+
